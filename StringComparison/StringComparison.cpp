0f1c388762da96d556091af5b351cc9ff0b9a603
Kris <kswhite609@hotmail.com>
2019-04-26 15:58:33 -0400

    Rev.1

--- StringComparison/StringComparison.cpp
+++ StringComparison/StringComparison.cpp
@@ -1,21 +1,25 @@
-// StringComparison.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "pch.h"
 #include <iostream>
+#include <string>
+#include <cstring>
 
+using namespace std;
+bool caseInsensitiveStringsAreEqual(const string& str1, const string& str2)
+{
+	if (str1.size() != str2.size())
+		return false;
+
+	for (int i = 0; i < str1.size(); i++)
+	{
+		if (toupper(str1[i]) != toupper(str2[i]))
+			return false;
+	}
+	return true;
+}
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	string str1 = "ABCD";
+	string str2 = "abce";
+	bool result = caseInsensitiveStringsAreEqual(str1, str2);
+    std::cout <<  boolalpha << result; 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
